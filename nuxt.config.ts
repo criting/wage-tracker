1de7f8f0424538c3ee003f7ad8f97b45882a00b8
Galabin Vasilev <galabinvasilev@Galabins-MacBook-Pro.local>
2025-07-04 13:38:16 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-lottie'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    'nuxt-lottie',
+    '@nuxthub/core'
+  ],
   css: ['~/assets/css/main.css'],
   future: {
     compatibilityVersion: 4
